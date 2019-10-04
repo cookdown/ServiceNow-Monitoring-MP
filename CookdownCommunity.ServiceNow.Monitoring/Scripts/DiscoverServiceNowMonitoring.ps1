cb8a428f87fa3e3cfb0d0779e47db311a4762271
Nathan Foreman <Nathan@Practice2Perfect.com>
2019-10-04 16:17:22 +0100

    Initial pack creation (#1)
    * Initial pack upload
    
    * Added health monitoring and account updates

---
+++ CookdownCommunity.ServiceNow.Monitoring/Scripts/DiscoverServiceNowMonitoring.ps1
@@ -0,0 +1,118 @@
+﻿#Bring in our parameters
+param($sourceID, $managedEntityID, $location, $instanceUrls, $computerName, $snowUserName, $snowPassword)
+
+Function CreateHeaders
+{
+	param($username, $password)
+	# Build auth header
+	$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $password)))
+
+	# Set proper headers
+	$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+	$headers.Add('Authorization',('Basic {0}' -f $base64AuthInfo))
+	$headers.Add('Accept','application/json')
+	$headers
+}
+
+Function GetClusterStatusFromServiceNow
+{
+    param($instanceURL)
+    $statsEndpoint = $instanceURL + "api/now/table/sys_cluster_state"
+    $results = Invoke-RESTMethod -Uri $statsEndpoint -Headers $headers -Method GET
+    $results.result
+}
+
+Function CreateInstanceObject
+{
+    param($sourceNode,$sourceURL)
+
+    $stats = [xml]$sourceNode.stats
+    $instance = $discoveryData.CreateClassInstance("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.ServiceNowInstance']$")
+    $instance.AddProperty("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.ServiceNowInstance']/URL$", $sourceURL)
+    $instance.AddProperty("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.ServiceNowInstance']/Name$", $Stats.xmlstats.instance_name)
+    $instance.AddProperty("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.ServiceNowInstance']/Type$", $Stats.xmlstats.instance_type)
+    $instance.AddProperty("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.ServiceNowInstance']/Version$", $Stats.xmlstats.'glide.build.name')
+    $instance.AddProperty("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.ServiceNowInstance']/Build$", $Stats.xmlstats.'glide.build.tag')
+    $instance.AddProperty("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.ServiceNowInstance']/DatabaseVersion$", $stats.xmlstats.'db.product_version')
+    $instance.AddProperty("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.ServiceNowInstance']/Offering$", $Stats.xmlstats.'glide.offering')
+    $instance.AddProperty("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.ServiceNowInstance']/POPStatus$", $stats.xmlstats.'glide.pop3.status'.InnerText)
+    $instance.AddProperty("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.ServiceNowInstance']/SMTPStatus$", $stats.xmlstats.'glide.smtp.status'.InnerText)
+	$instance.AddProperty("$MPElement[Name='System!System.Entity']/DisplayName$", $Stats.xmlstats.instance_name)
+    $instance
+}
+
+Function CreateNodeObject
+{
+    param($sourceNode,$sourceURL)
+
+    $stats = [xml]$sourceNode.stats
+    $node = $discoveryData.CreateClassInstance("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.ServiceNowNode']$")
+    $node.AddProperty("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.ServiceNowInstance']/URL$", $sourceURL)
+    $node.AddProperty("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.ServiceNowNode']/NodeId$", $sourceNode.node_id)
+    $node.AddProperty("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.ServiceNowNode']/SystemId$", $sourceNode.system_id)
+    $node.AddProperty("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.ServiceNowNode']/CreatedOn$", $sourceNode.sys_created_on)
+    $node.AddProperty("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.ServiceNowNode']/AllowInbound$", $sourceNode.allow_inbound)
+    $node.AddProperty("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.ServiceNowNode']/ReadyToFailover$", $sourceNode.ready_to_failover)
+	$node.AddProperty("$MPElement[Name='System!System.Entity']/DisplayName$", $sourceNode.system_id)
+    $node
+}
+
+Function CreateWatcherNode
+{
+    param($computerName,$sourceURL)
+    $watcherNode = $discoveryData.CreateClassInstance("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.WatcherNode']$")
+    $watcherNode.AddProperty("$MPElement[Name='Windows!Microsoft.Windows.Computer']/PrincipalName$", $computerName)
+    $watcherNode.AddProperty("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.WatcherNode']/Instance$", $sourceURL)
+	$watcherNode.AddProperty("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.WatcherNode']/InstanceUrlBase$", $sourceURL.Replace("https://","").Replace("/",""))
+    $watcherNode.AddProperty("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.WatcherNode']/Location$", $location)
+	$watcherNode.AddProperty("$MPElement[Name='System!System.Entity']/DisplayName$", $location)
+    $watcherNode
+}
+
+Function CreateWatcherRelationship
+{
+    param($instanceObject,$watcherObject)
+    $watcherRelationship = $discoveryData.CreateRelationshipInstance("$MPElement[Name='CookdownCommunity.ServiceNow.Monitoring.InstanceContainsWatchers']$")
+    $watcherRelationship.Source = $instanceObject
+    $watcherRelationship.Target = $watcherObject
+    $watcherRelationship
+}
+
+#Initialize the Object and create a discovery data unique to this Agent
+$api = New-Object -comObject 'MOM.ScriptAPI'
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$discoveryData = $api.CreateDiscoveryData(0, $sourceID, $managedEntityID)
+$headers = CreateHeaders -username $snowUserName -password $snowPassword
+
+
+
+foreach($instance in $instanceUrls.Trim().Split(","))
+{
+	$api.LogScriptEvent("SNOW Discovery", 1033, 4, $("working on cluster {0}." -f $instance))    
+	$instance = $instance.Trim().ToLower()
+    $clusterNodes = GetClusterStatusFromServiceNow -instanceURL $instance
+	$api.LogScriptEvent("SNOW Discovery", 1033, 4, $("retrieved cluster nodes count = {0}" -f $clusterNodes.count))    
+    # Discover instance
+    $serviceNowInstance = CreateInstanceObject -sourceNode $clusterNodes[0] -sourceURL $instance
+    $discoveryData.AddInstance($serviceNowInstance)
+	
+    #Add watcher
+    $watcherNode = CreateWatcherNode -computerName $computerName -sourceURL $instance
+    $discoveryData.AddInstance($watcherNode)
+	
+    #Relate watcher to instance
+    $watcherRelationship = CreateWatcherRelationship -instanceObject $serviceNowInstance -watcherObject $watcherNode
+    $discoveryData.AddInstance($watcherRelationship)
+	
+    #Add Nodes
+    foreach($snowNode in $clusterNodes)
+    {
+        $node = CreateNodeObject -sourceNode $snowNode -sourceURL $instance
+        $discoveryData.AddInstance($node)
+		$api.LogScriptEvent("SNOW Discovery", 1011, 4, $("Created node {0}" -f $snowNode.system_id))
+    }
+}
+
+$api.LogScriptEvent("SNOW Discovery", 1011, 4, $("Discovery Completed"))
+$discoveryData
---
+++ CookdownCommunity.ServiceNow.Monitoring/Scripts/ServiceNowStatusProbe.ps1
@@ -0,0 +1,165 @@
+﻿#Bring in our parameters
+param($instanceURL, $SnowUsername, $SnowPassword)
+
+Function CreateHeaders
+{
+	param($username, $password)
+	# Build auth header
+	$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username, $password)))
+
+	# Set proper headers
+	$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+	$headers.Add('Authorization',('Basic {0}' -f $base64AuthInfo))
+	$headers.Add('Accept','application/json')
+	$headers
+}
+
+Function MakeAddPerfPropertyBag
+{
+  param(
+    [string] $InstanceURL,
+    [string] $NodeId,
+    [string] $Object = "",
+    [string] $Instance = "",
+    [string] $Counter = "",
+    [double] $Value = $null
+    )
+    
+  if ($Value -ne $null) 
+  {
+    if ($Value -ne '')
+       {  
+		  $perfBag = $oAPI.CreatePropertyBag()
+		  $perfBag.AddValue("Type","Performance")
+		  $perfBag.AddValue("InstanceURL",$InstanceURL)
+		  $perfBag.AddValue("NodeId",$NodeId)
+		  $perfBag.AddValue("Object",$Object)
+		  $perfBag.AddValue("Instance",$Instance)
+		  $perfBag.AddValue("Counter",$Counter)
+		  $perfBag.AddValue("Value",$Value)
+		  $perfBag
+       }
+  }
+}
+
+Function MakeMonitoringStatusPropertyBag
+{
+	param(
+		[string] $InstanceURL,
+		[bool] $Success
+	)
+
+		$perfBag = $oAPI.CreatePropertyBag()
+		$perfBag.AddValue("Type","Monitoring")
+		$perfBag.AddValue("InstanceURL",$InstanceURL)
+		$perfBag.AddValue("Success",$Success.ToString())
+		$perfBag
+}
+
+Function GetClusterStatusFromServiceNow
+{
+    param($instanceURL)
+    $statsEndpoint = $instanceURL + "api/now/table/sys_cluster_state"
+    $results = Invoke-RESTMethod -Uri $statsEndpoint -Headers $headers -Method GET
+    $results.result
+}
+
+Function CreatePerformancePropertyBags
+{
+    param($node, $instance)
+    $ioStats = [xml]$node.iostats
+    $stats = [xml]$node.stats
+
+	$glidePool = $ioStats.xmlstats.iostats.pool | ? {$_.name -eq 'glide'}    
+
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Memory" -Counter "Total" -Instance $node.system_id -Value $stats.xmlstats.'system.memory.total'
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Memory" -Counter "In Use" -Instance $node.system_id -Value $stats.xmlstats.'system.memory.in.use'
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Memory" -Counter "Percent Free" -Instance $node.system_id -Value $stats.xmlstats.'system.memory.pct.free'
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Memory" -Counter "Max" -Instance $node.system_id -Value $stats.xmlstats.'system.memory.max'.InnerText
+
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Servlet" -Counter "Cache Flushes" -Instance $node.system_id -Value $stats.xmlstats.'servlet.cache.flushes'             
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Servlet" -Counter "Uptime" -Instance $node.system_id -Value $stats.xmlstats.'servlet.uptime'                    
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Servlet" -Counter "Transactions" -Instance $node.system_id -Value $stats.xmlstats.'servlet.transactions'              
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Servlet" -Counter "Errors Handled" -Instance $node.system_id -Value $stats.xmlstats.'servlet.errors.handled'            
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Servlet" -Counter "Processor Transactions" -Instance $node.system_id -Value $stats.xmlstats.'servlet.processor.transactions'    
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Servlet" -Counter "Cancelled Transactions" -Instance $node.system_id -Value $stats.xmlstats.'servlet.cancelled.transactions'    
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Servlet" -Counter "Active Sessions" -Instance $node.system_id -Value $stats.xmlstats.'servlet.active.sessions' 
+          
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Scheduler" -Counter "Worker Count" -Instance $node.system_id -Value $stats.xmlstats.'scheduler.worker.count'            
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Scheduler" -Counter "Queue Length" -Instance $node.system_id -Value $stats.xmlstats.'scheduler.queue.length'            
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Scheduler" -Counter "Mean Queue Age" -Instance $node.system_id -Value $stats.xmlstats.'scheduler.mean.queue.age'          
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Scheduler" -Counter "Total Jobs" -Instance $node.system_id -Value $stats.xmlstats.'scheduler.total.jobs'              
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Scheduler" -Counter "Total Burst Workers" -Instance $node.system_id -Value $stats.xmlstats.'scheduler.total.burst.workers'
+
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Glide Pool DB" -Counter "Selects" -Instance $node.system_id -Value $glidePool.select
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Glide Pool DB" -Counter "Selects (Over 1 Second)" -Instance $node.system_id -Value $glidePool.select_onesec
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Glide Pool DB" -Counter "Selects (Over 10 Sec)" -Instance $node.system_id -Value $glidePool.select_tensec
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Glide Pool DB" -Counter "Deletes" -Instance $node.system_id -Value $glidePool.delete
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Glide Pool DB" -Counter "Inserts" -Instance $node.system_id -Value $glidePool.insert
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Glide Pool DB" -Counter "Updates" -Instance $node.system_id -Value $glidePool.update
+
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "JVM" -Counter "Classes Loaded" -Instance $node.system_id -Value $stats.xmlstats.'jvm.classes.loaded'
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "JVM" -Counter "Classes Unloaded" -Instance $node.system_id -Value $stats.xmlstats.'jvm.classes.unloaded'
+
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Users" -Counter "End Users" -Instance $node.system_id -Value $stats.xmlstats.sessionsummary.end_user
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Users" -Counter "Logged In" -Instance $node.system_id -Value $stats.xmlstats.sessionsummary.logged_in
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId $node.node_id -Object "Users" -Counter "Total" -Instance $node.system_id -Value $stats.xmlstats.sessionsummary.total
+}
+
+
+try
+{
+	$oAPI = New-Object -comObject 'MOM.ScriptAPI'
+	$oAPI.LogScriptEvent("SNOW Performance", 1050, 4, $("Collecting performance for {0}" -f $InstanceURL))
+	[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+    $headers = CreateHeaders -username $SnowUsername -password $SnowPassword
+    $fetchTime = Measure-Command {$clusterNodes = GetClusterStatusFromServiceNow -instanceURL $instanceURL}
+
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId "NONE" -Object "Connection" -Counter "Fetch Time (MS)" -Instance "Total" -Value $fetchTime.TotalMilliseconds
+
+	# Setup our roll-up Performance
+	$instanceTotal_select = 0
+	$instanceTotal_select_onesec = 0
+	$instanceTotal_select_tensec = 0
+	$instanceTotal_delete = 0
+	$instanceTotal_insert = 0
+	$instanceTotal_update = 0
+	$instanceTotal_end_user = 0
+	$instanceTotal_logged_in = 0
+	$instanceTotal_total = 0
+
+    foreach($snowNode in $clusterNodes)
+    {
+        CreatePerformancePropertyBags -node $snowNode -instance $instance
+
+		# Update the node
+		$ioStats = [xml]$snowNode.iostats
+		$stats = [xml]$snowNode.stats
+		$glidePoolData = $ioStats.xmlstats.iostats.pool | ? {$_.name -eq 'glide'}   
+		$instanceTotal_select = $instanceTotal_select + $glidePoolData.select
+		$instanceTotal_select_onesec = $instanceTotal_select_onesec + $glidePoolData.select_onesec
+		$instanceTotal_select_tensec = $instanceTotal_select_tensec + $glidePoolData.select_tensec
+		$instanceTotal_delete = $instanceTotal_delete + $glidePoolData.delete
+		$instanceTotal_insert = $instanceTotal_insert + $glidePoolData.insert
+		$instanceTotal_update = $instanceTotal_update + $glidePoolData.update
+		$instanceTotal_end_user = $instanceTotal_end_user + $stats.xmlstats.sessionsummary.end_user
+		$instanceTotal_logged_in = $instanceTotal_logged_in + $stats.xmlstats.sessionsummary.logged_in
+		$instanceTotal_total = $instanceTotal_total + $stats.xmlstats.sessionsummary.total
+    }
+
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId "NONE" -Object "Glide Pool DB" -Counter "Selects" -Instance "Total"  -Value $instanceTotal_select
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId "NONE" -Object "Glide Pool DB" -Counter "Selects (Over 1 Second)" -Instance "Total" -Value $instanceTotal_select_onesec
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId "NONE" -Object "Glide Pool DB" -Counter "Selects (Over 10 Sec)" -Instance "Total" -Value $instanceTotal_select_tensec
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId "NONE" -Object "Glide Pool DB" -Counter "Deletes" -Instance "Total" -Value $instanceTotal_delete
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId "NONE" -Object "Glide Pool DB" -Counter "Inserts" -Instance "Total" -Value $instanceTotal_insert
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId "NONE" -Object "Glide Pool DB" -Counter "Updates" -Instance "Total" -Value $instanceTotal_update
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId "NONE" -Object "Users" -Counter "End Users" -Instance "Total" -Value $instanceTotal_end_user
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId "NONE" -Object "Users" -Counter "Logged In" -Instance "Total" -Value $instanceTotal_logged_in
+	MakeAddPerfPropertyBag -InstanceUrl $instance -NodeId "NONE" -Object "Users" -Counter "Total" -Instance "Total" -Value $instanceTotal_total
+
+    MakeMonitoringStatusPropertyBag -InstanceUrl -Success $true
+}
+catch
+{
+    MakeMonitoringStatusPropertyBag -InstanceUrl -Success $false
+}
